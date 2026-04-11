931d7c32e621efc7e3697d460aed935592862e1f
Leo Moe <moetheleo@gmail.com>
2026-04-11 17:13:03 +0200

    me and andreas finished the heuristic

--- tests/testmaterialcount.sv
+++ tests/testmaterialcount.sv
@@ -4,12 +4,10 @@ module testmaterialcount;
 
     state_t state;
     
-    color_t winning;
-    logic [15:0] value;
+    logic signed [31:0] value;
 
     materialcount dut(
         .state(state),
-        .winning(winning),
         .value(value)
     );
 /*
@@ -25,28 +23,14 @@ module testmaterialcount;
 
     initial begin
         $display("TEST:setting empty board and checking that it is fact zero.");
-        //clearinput();
 
-        state = '0;
-
-        #1
-
-        if(value!=0)begin
-            $display("the value should be 0 but it is %0d",value);
-        end
-
-        #1
-    
-        state.board[20].color = BLACK;
-        state.board[20].piece = QUEEN;
+        state = '0;    
+        state.board[20] = '{color:BLACK, piece:QUEEN};
 
         #1
 
-        if(winning!=BLACK)begin
-            $display("black should be winning but isn't?");
-        end
-        if(value!=9)begin
-            $display("value should be 9 but is %0d",value);
+        if(value != -900) begin
+            $display("value should be -900 but is %0d",value);
         end
 
         $display("TEST: inserting some material on both sides and then checking if the balance is right");
@@ -79,8 +63,7 @@ module testmaterialcount;
     state.board[54].piece=KING;
 */
 
-        if(winning!=BLACK)$display("black should be winning but isn't");
-        if(value!=5)$display("black advantage should be 5 but is %0d",value);
+        if(value!=0)$display("black advantage should be 0 but is %0d",value);
 
         #1
 
